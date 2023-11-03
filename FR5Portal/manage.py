8584dd5b27bb1d4e0f78752088ff6a32e9157a38
Zach <zacharydavidrice@gmail.com>
2023-11-03 18:59:17 -0400

    Database schema

--- FR5Portal/manage.py
+++ FR5Portal/manage.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from flask import Flask, jsonify, send_from_directory
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
+@app.route('/')
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FR5Portal.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+    data = {
+        test: 123
+    }
+    return jsonify(data)
